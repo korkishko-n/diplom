046a99d567f7b0eca8b0464da881c7f1da44448d
korkishko-n <130907675+korkishko-n@users.noreply.github.com>
2023-04-22 12:36:16 +0300

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,30 @@
-﻿
+﻿$(document).ready(function () {
+    // при клике на кнопку бургер меню показываем/скрываем меню
+    $('.burger-menu-btn').click(function () {
+        $('nav').slideToggle();
+    });
+
+    // при изменении размера экрана скрываем меню
+    $(window).resize(function () {
+        if ($(window).width() > 600) {
+            $('nav').removeAttr('style');
+        }
+    });
+});
+
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scrollTop = $(window).scrollTop();
+        var offset = $('.parallax').offset().top;
+        var height = $('.parallax').outerHeight();
+
+        // Если блок .parallax виден на экране, меняем позицию фонового изображения
+        if (offset + height > scrollTop && offset < scrollTop + $(window).height()) {
+            $('.parallax').css('background-position-y', (offset - scrollTop) / 2 + 'px');
+        }
+    });
+});
+document.querySelector('.burger-menu-btn').addEventListener('click', function () {
+    document.querySelector('nav').classList.toggle('show');
+});
